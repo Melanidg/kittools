4da853be2769df7cba84e130b998912bd46543ec
IsabellaSosa <sebastian.chavez1904@alumnos.udg.mx>
2025-08-26 16:14:03 -0600

    forms

--- kittools.py
+++ kittools.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 kittoolsApp = Flask(__name__)
 
 @kittoolsApp.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
+
+@kittoolsApp.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@kittoolsApp.route('/signin')
+def signin():
+    return render_template('signin.html')
 
 if __name__== '__main__':
     kittoolsApp.run(debug=True,port=3300)
